3b401808ca58fe563bb94f53b0b53ac254ebb81f
Jasim <abir_jasim@yahoo.com>
2024-04-28 16:14:00 +0100

    Title metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Metadata Absolute Testing",
+  },
+};
 
 const blog = () => {
   return <div>My Blog</div>;
